7ca200b25894d75be9606501f48bd602789f8448
Moisés C. Cuevas <moisesccuevas@gmail.com>
2025-04-29 13:48:16 -0600

    update

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -48,4 +48,77 @@ public class LinqQueries {
              where libro.PublishedDate.Year == year
              select libro;
     }
+
+    public bool AllBooksWerePublished() {
+      // expression method syntax
+      return libros.All(b => b.Status != string.Empty);
+    }
+
+    public bool AnyBookPublishedInYear(int year) {
+      // expression method syntax
+      return libros.Any(b => b.PublishedDate.Year == year);
+    }
+
+    public IEnumerable<Book> BooksWithCategory(string category) {
+      // expression method syntax
+      return libros.Where(b => b.Categories.Contains(category));
+    }
+
+    public IEnumerable<Book> BooksOrderedByCategoryAndTitle(string category) {
+      // expression method syntax
+      return libros.Where(b => b.Categories.Contains(category)).OrderBy(b => b.Title);
+    }
+
+    public IEnumerable<Book> TakeFirstThreeBooksPublishedRecentlyWithCategory( string category) {
+      // expression method syntax
+      return libros.Where(b => b.Categories.Contains(category)).OrderByDescending(b => b.PublishedDate).Take(3);
+    }
+
+    public IEnumerable<Book> BooksWithMoreThanPages(int pages) {
+      //return libros.Where(b => b.PageCount > pages).Take(4).Skip(2);
+      return libros.Where(b => b.PageCount > pages);
+    }
+
+    public IEnumerable<Book> TakeFirstThreeBooks() {
+      return libros.Take(3).Select(b => new Book {
+        Title = b.Title,
+        PageCount = b.PageCount
+      });
+    }
+
+    public int CountBooksWithPages(int min, int max) {
+      return libros.Count(b => b.PageCount >= min && b.PageCount <= max);
+    }
+
+    public DateTime GetOldestPublishedDate() {
+      return libros.Min(b => b.PublishedDate);
+    }
+
+    public int GetHighestPageCount() {
+      return libros.Max(b => b.PageCount);
+    }
+
+    public Book GetBookWithLowestPageCount() {
+      return libros.Where(b => b.PageCount > 0).MinBy(b => b.PageCount) ?? new Book();
+    }
+
+    public int SumBooksWithPageCountBetween(int min, int max) {
+      return libros.Where(b => b.PageCount >= min && b.PageCount <= max).Sum(b => b.PageCount);
+    }
+
+    public string BooksTitlesConcatenatedWithPublicationDateAfter(int year) {
+      return libros.Where(b => b.PublishedDate.Year > year).Aggregate("", (titulos, next) => titulos + ", " + next.Title);
+    }
+
+    public IEnumerable<IGrouping<int, Book>> GourpByBooksPulishedAfter(int year) {
+      return libros.Where(b => b.PublishedDate.Year > year).GroupBy(b => b.PublishedDate.Year);
+    }
+
+    public ILookup<char, Book> DictionaryBooksByFirstLetter() {
+      return libros.ToLookup(b => b.Title[0], b => b);
+    }
+
+    public IEnumerable<Book> JoinBooksBetweenTwoLists(IEnumerable<Book> books1, IEnumerable<Book> books2) {
+      return books1.Join(books2, b1 => b1.Title, b2 => b2.Title, (b1, b2) => b1);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,37 @@
 
 var linqQueries = new LinqQueries();
 // var allBooks = linqQueries.AllBooks();
-var libros = linqQueries.BooksPublisedAfterYear(2000);
+//var books = linqQueries.BooksPublisedAfterYear(2000);
 
-linqQueries.PrintBooks(libros);
+// all books has status not empty
+var allBooks = linqQueries.AllBooksWerePublished();
+Console.WriteLine($"All books has status not empty: {allBooks}");
+
+// any book published in 2005
+var anyBook = linqQueries.AnyBookPublishedInYear(2005);
+Console.WriteLine($"Any book published in 2005: {anyBook}");
+
+//var books = linqQueries.BooksWithCategory("Python");
+//Console.WriteLine($"Books with category Python: \n");
+
+//var books = linqQueries.BooksOrderedByCategoryAndTitle("Java");
+//Console.WriteLine($"Books ordered by category and title: \n");
+
+//var books = linqQueries.TakeFirstThreeBooksPublishedRecentlyWithCategory("Java");
+
+Console.WriteLine($"Books with pages between 200 and 500: {linqQueries.CountBooksWithPages(200, 500)} \n");
+
+Console.WriteLine($"The oldest publication date: {linqQueries.GetOldestPublishedDate()} \n");
+
+Console.WriteLine($"The highest page count: {linqQueries.GetHighestPageCount()} \n");
+
+Console.WriteLine($"Book with the lowest page count: {linqQueries.GetBookWithLowestPageCount().Title} \n");
+
+Console.WriteLine($"Titles of books with published date after 2015: {linqQueries.BooksTitlesConcatenatedWithPublicationDateAfter(2015)} \n");
+
+var books2 = linqQueries.BooksWithMoreThanPages(500);
+var books1 = linqQueries.BooksPublisedAfterYear(2005);
+
+var joinedBooks = linqQueries.JoinBooksBetweenTwoLists(books1, books2);
+
+linqQueries.PrintBooks(joinedBooks);
